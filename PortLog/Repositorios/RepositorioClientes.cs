34c3753a81c3348f88f0f7ace3f51dd028627939
Manuel <ManuelBertinat47@gmail.com>
2020-05-12 17:14:38 -0300

    Repositorio Usuarios

---
+++ PortLog/Repositorios/RepositorioClientes.cs
@@ -0,0 +1,14 @@
+﻿using Dominio.EntidadesNegocio;
+using Dominio.InterfazRepositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorios
+{
+    class RepositorioClientes
+    {
+    }
+}
